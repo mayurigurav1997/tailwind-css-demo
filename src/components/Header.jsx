57e87fc74706015ef931ecc73e685c5f61d6039d
Mayuri <mayurigurav1997@gmail.com>
2024-03-13 17:03:37 +0530

    fontfamily added in extended

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
       <img src={logo} alt="A canvas" className="mb-8 w-44 h-44 object-contain" />
       <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>ReactArt</h1>
       {/* <p className={classes.paragraph}>A community of artists and art-lovers.</p> */}
-      <p>A community of artists and art-lovers.</p>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
